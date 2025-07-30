62f249c1753e7a0f1451e347755370622bcc420d
clementAvesque <clement2avesque@gmail.com>
2025-07-30 13:09:54 +0200

    finission du responsive

--- page/slugpage/slugpage.js
+++ page/slugpage/slugpage.js
@@ -47,7 +47,7 @@ explain[1].innerHTML = projet.delais
 let contenu = document.getElementById("content")
 if (Array.isArray(projet.content_presentation)) {
     projet.content_presentation.forEach(element => {
-        contenu.style.width = "60%"
+
         let Content_width = (contenu.offsetWidth - (50 * projet.content_presentation.length)) / projet.content_presentation.length
         let iframe = document.createElement("iframe");
         iframe.width = `${Content_width}px`;
@@ -63,18 +63,22 @@ if (Array.isArray(projet.content_presentation)) {
 
 } else {
 
-    if (isExternalLink(projet.content_presentation)) {
-        let iframe = document.createElement("iframe");
-        iframe.width = "1000px";
-        iframe.height = "600px"
-        iframe.style.marginTop = `10%`
-        iframe.src = projet.content_presentation;
-        iframe.title = "YouTube video player";
-        iframe.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share";
-        iframe.referrerPolicy = "strict-origin-when-cross-origin";
-        iframe.allowFullscreen = true;
-        contenu.appendChild(iframe);
-    } else {
+if (isExternalLink(projet.content_presentation)) {
+    let iframe = document.createElement("iframe");
+    if (window.matchMedia("(max-width: 800px)").matches) {
+        // Mode téléphone
+        iframe.width = "90vw";
+        iframe.height = "50vw";
+        iframe.style.maxWidth = "100%";
+        iframe.style.marginTop = "10%";
+    }
+    iframe.src = projet.content_presentation;
+    iframe.title = "YouTube video player";
+    iframe.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share";
+    iframe.referrerPolicy = "strict-origin-when-cross-origin";
+    iframe.allowFullscreen = true;
+    contenu.appendChild(iframe);
+} else {
         img = document.createElement("img")
         img.src = `../../${projet.content_presentation}`
         contenu.appendChild(img);
@@ -91,7 +95,9 @@ document.getElementById("Demarche").appendChild(demarche)
 projet.logiciel.forEach(logiciel => {
     let image = document.createElement("img")
     image.src = `../../${logiciel}`
-    document.getElementById("image").appendChild(image)
+    document.querySelectorAll(".image").forEach(object =>{
+        object.appendChild(image.cloneNode())
+    })
 
 })
 
